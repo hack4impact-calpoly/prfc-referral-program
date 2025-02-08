084b02da7a9979049a12ebd9ffa1098e3c702e8e
Tylermhh <tyler.hlaing@gmail.com>
2025-02-08 14:05:55 -0800

    get req handler working

--- src/app/api/referral/route.ts
+++ src/app/api/referral/route.ts
@@ -14,3 +14,21 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ message: "Error creating referral." }, { status: 500 });
   }
 }
+
+// To use whenever you fetch something from the db and want to return it
+// Resolves the error "TypeError: Do not know how to serialize a BigInt"
+function bigIntReplacer(key: string, value: any) {
+  return typeof value === "bigint" ? value.toString() : value;
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    // Fetch all referrals from the referral table
+    const referrals = await prisma.referral.findMany();
+
+    return NextResponse.json(JSON.parse(JSON.stringify(referrals, bigIntReplacer)), { status: 200 });
+  } catch (err) {
+    console.error("Error fetching referrals:", err);
+    return NextResponse.json({ message: "Error fetching referrals." }, { status: 500 });
+  }
+}
