1dc49c45d277dc2586c16c5f0a12518d5dd75693
Tylermhh <tyler.hlaing@gmail.com>
2025-02-24 20:33:34 -0800

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/Navbar";
 import ReferralForm from "@/components/ReferralForm/ReferralForm";
-import ReferralDatabase from "./referralDatabase/page";
 
 export default function Home() {
   return (
--- src/app/referralDatabase/page.tsx
+++ src/app/referralDatabase/page.tsx
@@ -1,16 +1,6 @@
 import ReferralDataGrid from "@/components/ReferralDatabase/ReferralDataGrid";
 import Header from "@/components/Header/Header";
 import styles from "./referralDatabase.module.css";
-import localFont from "next/font/local";
-
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// // Load font manually
-// const komikaAxis = localFont({
-//   src: "../../public/fonts/KomikaAxis.ttf",
-//   display: "swap",
-// });
 
 //referral page to display user data
 export default function ReferralDatabase() {
