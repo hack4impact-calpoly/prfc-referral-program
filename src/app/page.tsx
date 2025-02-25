dbf1eb7882c0e3c04792478270123618c2fed4c2
Tylermhh <tyler.hlaing@gmail.com>
2025-02-24 20:29:05 -0800

    modified home back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ import ReferralDatabase from "./referralDatabase/page";
 export default function Home() {
   return (
     <main>
-      {/*<Navbar />*/}
-      {/*<h1>Home</h1>*/}
-      {/*<ReferralForm></ReferralForm>*/}
-
-      <ReferralDatabase />
+      <Navbar />
+      <h1>Home</h1>
+      <ReferralForm></ReferralForm>
     </main>
   );
 }
