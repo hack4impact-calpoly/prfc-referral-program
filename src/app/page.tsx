08d00a69be1dd17f0620395d34eb426c54bcf7f0
Karthik Balaji <karthikb0606@gmail.com>
2025-03-06 00:31:03 -0800

    light main page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,54 @@
 import Navbar from "@/components/Navbar";
+import Header from "@/components/Header/Header";
 import ReferralForm from "@/components/ReferralForm/ReferralForm";
+import styles from "@/styles/page.module.css";
+import Image from "next/image";
+import { Luckiest_Guy } from "next/font/google";
+
+const luckiestGuy = Luckiest_Guy({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <Navbar />
-      <h1>Home</h1>
-      <ReferralForm></ReferralForm>
+      <Header />
+
+      {/* Hero Section */}
+      <section className={styles.hero}>
+        <div className={styles.heroText}>
+          <div className={luckiestGuy.className}>
+            <h1>Invite Others to Join the Co-Op!</h1>
+            <h2>Refer a Family Member or Friend</h2>
+          </div>
+        </div>
+        <div className={styles.heroImage}>
+          <Image
+            src="/produce.jpg"
+            alt="Fresh Produce"
+            width={500} // set the desired width
+            height={500} // set the desired height
+            layout="intrinsic" // use intrinsic layout for fixed size images
+          />
+        </div>
+      </section>
+
+      {/* Referral Form */}
+      <ReferralForm />
+
+      {/* Why Refer & Referral History */}
+      <section className={styles.referralSection}>
+        <div className={styles.leftSection}>
+          <div className={luckiestGuy.className}>
+            <h2>Why Should I Refer?</h2>
+            <p>By referring others, you can help our community reach its goal of hitting ___ members!</p>
+            <h2>Prizes</h2>
+            <p>For every referral, you gain one __________.</p>
+          </div>
+        </div>
+        <div className={styles.rightSection}>
+          <h2>Referral History</h2>
+        </div>
+      </section>
     </main>
   );
 }
